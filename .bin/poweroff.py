89eeb5fc16f62616549b5b624b6d5ef5d1389580
redpfire <redpixafire@gmail.com>
2020-01-30 00:41:19 +0100

    .bin: Add new scripts

---
+++ .bin/poweroff.py
@@ -0,0 +1,13 @@
+#!/bin/env python3
+import os, subprocess
+fd = os.open('/dev/input/event3', 0)
+while True:
+    os.read(fd, 512)
+    try:
+        r = subprocess.check_output('dmenu -b -p "Power off" -nb "#101010" -sb "#ff0000" <<< `printf "Abort\\nPoweroff\\nReboot"`', shell=True).strip().decode('utf8')
+        if r == 'Poweroff':
+            os.system('poweroff')
+        elif r == 'Reboot':
+            os.system('reboot')
+    except:
+        pass
