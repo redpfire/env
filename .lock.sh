dafc4323e8ba778a5a84f74e6ca07abfa5eee967
redpfire <redpixafire@gmail.com>
2020-02-29 20:55:34 +0100

    Update

---
+++ .lock.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+ICON=$HOME/.lock.icon.png
+TMPBG=/tmp/screen.png
+#scrot -e "mv \$f $TMPBG"
+#xset dpms force off
+#pkill compton
+sleep 1
+#compton --dbus -Gf &
+#convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG -scale 25% -scale 400% $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+#$(sleep 0.06 && xset dpms force on) &
+#disown
+#i3lock -u -i $TMPBG --debug
+#sleep 1
+#pkill compton
