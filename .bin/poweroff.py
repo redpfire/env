d9af14f3e290071a85a274e211a07f63e721f5dd
redpfire <redpixafire@gmail.com>
2020-03-21 04:08:05 +0100

    .bin/poweroff.py: Fix when /bin/sh is not bash

--- .bin/poweroff.py
+++ .bin/poweroff.py
@@ -4,7 +4,7 @@ fd = os.open('/dev/input/event3', 0)
 while True:
     os.read(fd, 512)
     try:
-        r = subprocess.check_output('dmenu -b -p "Power off" -nb "#101010" -sb "#ff0000" <<< `printf "Abort\\nPoweroff\\nReboot"`', shell=True).strip().decode('utf8')
+        r = subprocess.check_output('dmenu -b -p "Power off" -nb "#101010" -sb "#ff0000" <<< `printf "Abort\\nPoweroff\\nReboot"`', shell=True, executable="/bin/bash").strip().decode('utf8')
         if r == 'Poweroff':
             os.system('poweroff')
         elif r == 'Reboot':
